3cc7535fb666f6750305b787dcf7ea0d7ab6b610
Internaut007 <noreply>
2026-03-01 11:50:19 +0100

    Translations completet

--- internautenb2boffer/translations/fr.php
+++ internautenb2boffer/translations/fr.php
@@ -3,9 +3,22 @@
 global $_MODULE;
 $_MODULE = [];
 
-$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_daaa5458fa110d4cec339379c3bda347'] = 'Internauten B2BDemander une offre';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_daaa5458fa110d4cec339379c3bda347'] = 'Internauten B2B Demander une offre';
 $_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_5265a8ad736c7c0368051dae5bc5df00'] = 'Permet aux clients de passer commande et de demander une offre.';
 $_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_876f23178c29dc2552c0b48bf23cd9bd'] = 'Êtes-vous sûr de vouloir désinstaller ce module ?';
-$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_58c85aa11cba87114d4cb4a305406ee6'] = 'En attente de l\'offre';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_f38f5974cdc23279ffe6d203641a8bdf'] = 'Paramètres mis à jour.';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_8bc639f4af1fedabd1b95337b6b6bbd7'] = 'Les journaux de débogage sont actuellement activés.';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_a97ea05b1e9a155a91ffb02981c4ba50'] = 'Les journaux de débogage sont actuellement désactivés.';
 $_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_f81ee7355de99c6263d4582719b00d8a'] = 'Offre acceptée';
 $_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_1d3bb92700ca25028c3edfb675c9f0dd'] = 'Offre rejetée';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_65b849a361f21284abc6df2a7437965c'] = 'Paramètres de débogage';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_1b53492c6676712f7449f3fb47976f0c'] = 'Activer les journaux de débogage';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_dbd5c10dc428851a79ecb9a3e9dbe220'] = 'Écrire les traces de débogage du module dans les journaux du Back Office.';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activé';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_b9f5c797ebbf55adccdd8539a65a0241'] = 'Désactivé';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{internautenb2boffer}prestashop>internautenb2boffer_58c85aa11cba87114d4cb4a305406ee6'] = 'En attente d\'une offre';
+$_MODULE['<{internautenb2boffer}prestashop>payment_option_a39abf09b862357065b8714d3c72638d'] = 'Nous examinerons votre demande et vous enverrons une offre personnalisée. Votre commande sera enregistrée avec le statut « En attente ».';
+$_MODULE['<{internautenb2boffer}prestashop>payment_return_da3383165a628653acc588a8a832d4c5'] = 'Votre demande a bien été reçue.';
+$_MODULE['<{internautenb2boffer}prestashop>payment_return_3dd42bfc8345e82c43b4a40a3426ec49'] = 'Référence :';
+$_MODULE['<{internautenb2boffer}prestashop>payment_return_66c4c5112f455a19afde47829df363fa'] = 'Total :';
